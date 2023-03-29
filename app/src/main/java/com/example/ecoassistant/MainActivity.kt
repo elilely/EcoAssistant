3549f46e1be79744691fa59fcec527e8088ebeb9
elilely <elinamagomedova101@gmail.com>
2023-03-29 22:27:11 +0300

    EcoAssistant App

--- app/src/main/java/com/example/ecoassistant/MainActivity.kt
+++ app/src/main/java/com/example/ecoassistant/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.ecoassistant
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.ecoassistant.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 
@@ -11,22 +15,44 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var drawer: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-        firebaseAuth = FirebaseAuth.getInstance()
-        binding.signOutButton.setOnClickListener {
-            firebaseAuth.signOut()
-            val intent = Intent(this, StartActivity::class.java)
-            startActivity(intent)
+        drawer = findViewById(R.id.drawer_layout)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+/*binding = ActivityMainBinding.inflate(layoutInflater)
+setContentView(binding.root)
+
+firebaseAuth = FirebaseAuth.getInstance()
+binding.signOutButton.setOnClickListener {
+    firebaseAuth.signOut()
+    val intent = Intent(this, StartActivity::class.java)
+    startActivity(intent)
+}*/
+
+/*supportActionBar?.setDefaultDisplayHomeAsUpEnabled(true)
+supportActionBar?.title = "Eco Asistents"*/
+    }
+
+    override fun onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START)
+        } else{
+            super.onBackPressed()
         }
-        /*supportActionBar?.setDefaultDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = "Eco Asistents"*/
     }
 
 }
